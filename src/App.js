98172b8894a5fce20a1bd890d5749bddccf0b0a5
Elysia Darbourne <elysiadarbourne@yahoo.ca>
2022-06-25 17:57:49 -0400

    all changes

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import Form from './Form';
 class App extends Component {
   render () {
   return [
+    <header> 
       <div className="header">
       Summer Fling
-      </div>,
+      </div>
+    </header>,
       <Form />
       ];
   }
