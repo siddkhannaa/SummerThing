c425ae0f4ed0270a1dfc0a0ebdb7a931d28a7cfb
ide-k <siddharthkhanna2k@gmail.com>
2022-06-26 00:52:06 -0400

    resolve pr requests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('render person 1 text box in app', () => {
+test('render page title', () => {
   render(<App />);
   const placeholder = screen.getByText(/Summer Fling/i);
   expect(placeholder).toBeInTheDocument();
